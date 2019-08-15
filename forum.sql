b90d758465d7b475be688e3f5633260cb408846e
vagrant <vagrant@vagrant.vm>
2019-08-15 04:00:42 +0000

    three commit

--- forum.sql
+++ forum.sql
@@ -1,5 +1,8 @@
 
-CREATE TABLE posts ( content TEXT,
-                     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                     id SERIAL );
+CREATE TABLE products ( id INTEGER PRIMARY KEY,
+                     price NUMERIC(15,6),
+                     name TEXT);
 
+CREATE TABLE sales ( id INTEGER REFERENCES products,
+                      sale_date DATE NOT NULL DEFAULT CURRENT_DATE,
+                      count INTEGER);
